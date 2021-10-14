28d1bc4fd6bd69188373514eea844961c40fdeed
hbisevac1 <hbisevac1@etf.unsa.ba>
2021-10-14 16:41:56 +0200

    Initial commit

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -3,7 +3,7 @@ package ba.unsa.etf.rpr;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Java program probaaa po hiljaditi put");
+        System.out.println("Java program probaaa po 100000 put");
 	// write your code here
     }
 }
